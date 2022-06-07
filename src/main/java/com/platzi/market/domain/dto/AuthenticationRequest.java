f041bf572e648b69b57447bbbd832761b7ab6d07
Josue Flores <josue5325@gmail.com>
2022-06-07 18:16:03 -0500

    Se agrega la configuracion de JsonWebTpken para que autentique el usuario con un token con tiempo de expedicion

---
+++ src/main/java/com/platzi/market/domain/dto/AuthenticationRequest.java
@@ -0,0 +1,22 @@
+package com.platzi.market.domain.dto;
+
+public class AuthenticationRequest {
+    private String ussername ;
+    private String password ;
+
+    public String getUssername() {
+        return ussername;
+    }
+
+    public void setUssername(String ussername) {
+        this.ussername = ussername;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
